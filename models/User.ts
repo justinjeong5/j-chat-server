f1d4078fe698153c434f939b84ae6594fcba9cdd
Justin Jeong <justin.jeong@buzzvil.com>
2023-09-11 22:10:14 +0900

    add(db): j-chat

--- models/User.ts
+++ models/User.ts
@@ -9,31 +9,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-User.create([
-    {
-        username: "A",
-        email: "a@gmail.com",
-        password: "123456",
-        avatar: "https://i.imgur.com/3xXWYUu.png",
-    },
-    {
-        username: "B",
-        email: "b@gmail.com",
-        password: "123456",
-        avatar: "https://i.imgur.com/3xXWYUu.png",
-    },
-    {
-        username: "C",
-        email: "c@gmail.com",
-        password: "123456",
-        avatar: "https://i.imgur.com/3xXWYUu.png",
-    },
-    {
-        username: "D",
-        email: "d@gmail.com",
-        password: "123456",
-        avatar: "https://i.imgur.com/3xXWYUu.png",
-    },
-]);
-
 export default User;
--- routes/user.ts
+++ routes/user.ts
@@ -2,14 +2,37 @@ import express from "express";
 
 import User from "../models/User";
 
-const app = express();
+const R = express();
 
-app.use("/users", async (req, res) => {
-    const docs = await User.find({ name: "A" }).exec();
-    console.log(docs);
+R.get("/users", async (req, res) => {
+    const docs = await User.find();
+
     res.status(200).json({
         data: docs,
     });
 });
 
-export default app;
+R.post("/users", async (req, res) => {
+    const { username, email, password, avatar } = req.body;
+    const userExist = await User.find({ username }).exec();
+
+    if (userExist.length) {
+        res.status(400).json({
+            message: "User already exists",
+        });
+        return;
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+        avatar,
+    });
+    const doc = await user.save();
+    res.status(201).json({
+        data: doc,
+    });
+});
+
+export default R;
