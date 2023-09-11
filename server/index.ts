b6307f6ab57298e07b0122f14cabc86bb589395e
Justin Jeong <justin.jeong5@gmail.com>
2023-09-11 18:21:13 +0900

    fix(db): Mongodb connection (#7)

--- server/index.ts
+++ server/index.ts
@@ -1,36 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import dotenv from "dotenv";
 import express from "express";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
 
 import initRouter from "../routes";
 
 dotenv.config();
 const app = express();
-const port = 3005;
 
-const client = new MongoClient(process.env.MONGODB_URL as string, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log(
-            "Pinged your deployment. You successfully connected to MongoDB!",
-        );
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("successfully connected to database");
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 initRouter(app);
 
@@ -38,6 +23,7 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+const port = 3005;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
