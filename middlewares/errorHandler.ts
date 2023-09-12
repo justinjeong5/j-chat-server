86a906750f28f757d5c2956f52df511182e8414f
Justin Jeong <justin.jeong@buzzvil.com>
2023-09-12 14:59:02 +0900

    WIP

--- middlewares/errorHandler.ts
+++ middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
-import { Error } from "@types/error.type";
 import { Application, NextFunction, Request, Response } from "express";
+
+import { Error } from "../types/error.type";
 
 const errorHandlerMiddleware = (app: Application): void => {
     app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
--- routes/user.ts
+++ routes/user.ts
@@ -3,15 +3,35 @@ import express, { Request, Response } from "express";
 
 const R = express();
 
-R.get("/users", async (req: Request, res: Response) => {
-    const docs = await User.find();
+R.patch(
+    "/users/:userId",
+    async (req: Request, res: Response): Promise<void> => {
+        const [user] = await User.find({ _id: req.params.userId }).exec();
+        if (!user) {
+            res.status(404).json({
+                message: "User not found",
+            });
+            return;
+        }
+        await User.findOneAndUpdate({ id: user.id }, req.body);
+        const [user3] = await User.find({ id: user.id });
+        res.status(200).json({ data: user3 });
+    },
+);
 
-    res.status(200).json({
-        data: docs,
-    });
+R.get("/users/:userId", async (req: Request, res: Response): Promise<void> => {
+    const [user] = await User.find({ _id: req.params.userId }).exec();
+    if (!user) {
+        res.status(404).json({
+            message: "User not found",
+        });
+        return;
+    }
+
+    res.status(200).json({ data: user });
 });
 
-R.post("/users", async (req: Request, res: Response) => {
+R.post("/users", async (req: Request, res: Response): Promise<void> => {
     const { username, email, password, avatar } = req.body;
     const userExist = await User.find({ username }).exec();
 
@@ -31,6 +51,14 @@ R.post("/users", async (req: Request, res: Response) => {
     const doc = await user.save();
     res.status(201).json({
         data: doc,
+    });
+});
+
+R.get("/users", async (req: Request, res: Response): Promise<void> => {
+    const docs = await User.find();
+
+    res.status(200).json({
+        data: docs,
     });
 });
 
