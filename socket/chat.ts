22f19d4829e971547235f7c21e147197e1067a31
Justin Jeong <justin.jeong@buzzvil.com>
2023-10-30 21:53:41 +0900

    WIP

--- socket/chat.ts
+++ socket/chat.ts
@@ -1,27 +1,30 @@
+socket.on("submitMessage", data => {});
 
-
-socket.on('submitMessage', (data) => {
-    
-  })
-
-  connect.then(database => {
+connect.then(database => {
     const chat = new Chat(data);
     chat.save((error, doc) => {
-      if (error) {
-        console.error(error);
-        return io.emit('SocketError', { message: '채팅을 저장하는 과정에서 문제가 발생했습니다.', error })
-      }
-      Chat.findOne({ '_id': doc._doc._id })
-        .populate('writer')
-        .exec((error, chat) => {
-          if (error) {
+        if (error) {
             console.error(error);
-            return io.emit('SocketError', { message: '채팅을 불러오는 과정에서 문제가 발생했습니다.', error })
-          }
-          const fullChat = chat.toJSON();
-          delete fullChat.writer.password;
-          delete fullChat.writer.token;
-          return io.emit('returnMessage', { chat: fullChat })
-        })
-    })
-  })
+            return io.emit("SocketError", {
+                message: "채팅을 저장하는 과정에서 문제가 발생했습니다.",
+                error,
+            });
+        }
+        Chat.findOne({ _id: doc._doc._id })
+            .populate("writer")
+            .exec((error, chat) => {
+                if (error) {
+                    console.error(error);
+                    return io.emit("SocketError", {
+                        message:
+                            "채팅을 불러오는 과정에서 문제가 발생했습니다.",
+                        error,
+                    });
+                }
+                const fullChat = chat.toJSON();
+                delete fullChat.writer.password;
+                delete fullChat.writer.token;
+                return io.emit("returnMessage", { chat: fullChat });
+            });
+    });
+});
--- socket/index.ts
+++ socket/index.ts
@@ -1,17 +1,28 @@
 import Logger from "@lib/logger";
 import { Application } from "express";
-import http from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 
 export default function initSocket(app: Application): void {
-    const server = http.createServer(app);
-    const io = new Server(server);
-    io.on("connection", socket => {
-        socket.on("submitMessage", data => {
-            console.log(data);
+    const server = createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: "http://localhost:3000",
+            credentials: true,
+        },
+    });
+    io.on("connection", client => {
+        console.log("Socket Connection", client.id);
+        client.on("submitMessage", data => {
+            console.log("Socket submitMessage", data);
             return io.emit("returnMessage", { chat: data });
+        });
+        client.on("hello", data => {
+            console.log("Socket hello", data);
+            return io.emit("world", { chat: data });
         });
     });
 
+    server.listen(3006);
     Logger.done("successfully loaded socket.io");
 }
