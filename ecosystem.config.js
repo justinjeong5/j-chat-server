840dbd77798e751b97fae5751e358ddcc1b2cc71
justinjeong5 <justin.jeong5@gmail.com>
2024-01-14 16:09:02 +0900

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,17 +7,4 @@ module.exports = {
             exec_mode: "cluster",
         },
     ],
-    // deploy: {
-    //     production: {
-    //         user: "SSH_USERNAME",
-    //         host: "SSH_HOSTMACHINE",
-    //         ref: "origin/master",
-    //         repo: "GIT_REPOSITORY",
-    //         path: "DESTINATION_PATH",
-    //         "pre-deploy-local": "",
-    //         "post-deploy":
-    //             "npm install && pm2 reload ecosystem.config.js --env production",
-    //         "pre-setup": "",
-    //     },
-    // },
 };
