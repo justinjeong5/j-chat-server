bfe8480b96f11f3563ef9866c123c3806c3c215f
justinjeong5 <justin.jeong5@gmail.com>
2024-02-05 19:15:57 +0900

    중복되는 로직 제거

--- src/socket/user.ts
+++ src/socket/user.ts
@@ -1,21 +1,25 @@
 import User from "@models/User";
 import { Server, Socket } from "socket.io";
 
-export function userStatus(io: Server, client: Socket) {
-    client.on("userLogin", async (data: any) => {
+export function userLogin(io: Server, client: Socket) {
+    client.on("emitLogin", async (data: any) => {
         await User.findByIdAndUpdate(data.id, {
             $set: { active: true },
         }).exec();
-        io.emit("userLogin", { user: data.id });
+        io.emit("returnLogin", { user: data.id });
     });
-    client.on("userLogout", async (data: any) => {
+}
+
+export function userLogout(io: Server, client: Socket) {
+    client.on("emitLogout", async (data: any) => {
         await User.findByIdAndUpdate(data.id, {
             $set: { active: false },
         }).exec();
-        io.emit("userLogout", { user: data.id });
+        io.emit("returnLogout", { user: data.id });
     });
 }
 
 export default function registerUserSocket(io: Server, socket: Socket) {
-    userStatus(io, socket);
+    userLogin(io, socket);
+    userLogout(io, socket);
 }
